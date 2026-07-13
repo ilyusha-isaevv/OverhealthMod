c7c54b4ba373404e0700ca7f3d325c1d30f9a95f
Ilya Isaev <ilyusha.isaevv@gmail.com>
2026-07-13 13:15:39 +0300

    upd: localization for en-US

--- Common/Configs/GameplayConfig.cs
+++ Common/Configs/GameplayConfig.cs
@@ -8,15 +8,18 @@ public class GameplayConfig : ModConfig
 {
     public override ConfigScope Mode => ConfigScope.ServerSide;
 
-    [Range(1f, 3f)]
-    [DefaultValue(1f)]
-    public float OverhealthDecreaseRateMutiplier { get; set; }
-
     [Range(1.5f, 10f)]
+    [Increment(0.1f)]
     [DefaultValue(2f)]
     public float ConstantDecreaseRate { get; set; }
 
     [Range(4f, 20f)]
+    [Increment(0.1f)]
     [DefaultValue(5f)]
     public float ProgressiveDecreaseRate { get; set; }
+
+    [Range(1f, 3f)]
+    [Increment(0.1f)]
+    [DefaultValue(1f)]
+    public float OverhealthDecreaseRateMutiplier { get; set; }
 }
